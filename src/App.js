2a3f55668fed61d27ddf9882a600d47045e01e0a
ajay1478 <109083733+ajay1478@users.noreply.github.com>
2022-07-12 11:58:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Header from './Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Header from './header';
+
+function App() {
+  return (
+    
+
+        <Header/>
+    
+  
+
+    
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,30 @@
+function Header(){
+    return(
+    
+        <div>
+
+
+        <div className="container">  
+            <div>
+               <div>
+                <ul>
+                    <li>home</li>
+                    <li>shop</li>
+                    <li>about us</li>
+                    <li>contact</li>
+                </ul>
+               </div> 
+               <div>
+                <img src= {require('./image/asset 2.png')} alt="" />
+
+               </div>
+               
+
+               </div>
+        </div>
+            
+        
+</div>
+    );
+}
+export default Header;
